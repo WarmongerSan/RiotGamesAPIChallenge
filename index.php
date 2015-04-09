8570a5aefad899f375e89188fc84c20bf9489c32
Ainsley Wheeler <jemoedertje123123@hotmail.com>
2015-04-09 12:23:09 +0200

    Put a form in the page

--- index.php
+++ index.php
@@ -12,68 +12,67 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <button>Back to the beginning</button>
-        <button id="Pause" style="display:none;">Pause</button>
-        <button id="Play" onclick="javascript: listTicker({list: eventList ,startIndex:0,trickerPanel: $('#events'),interval: 3 * 50,});">Play</button>
-        <button id="Play2" style="display:none;">Play</button>
-        <button>Next Player</button>
-        <div id="events"></div>
+        <form action=" " method="post">
+            <input type="text" name="summoner" placeholder="Summoner Name" />
+        </form>
         <?php
-            $allmatches = get_object_vars(json_decode(file_get_contents("https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/382991?api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
-            foreach($allmatches as $matches){
-                foreach($matches as $match){
-                    $match = get_object_vars($match);
-                    //if($match['queueType'] === "URF_5x5"){
-                        echo "<br /><br />" . $match['queueType'] . "<br />";
-                        echo "<br /><br />" . $match['queueType'] . "<br />";
-                        print_r(array_values($match));
-                        
-                    //}
+            if(isset($_POST['summoner'])){
+                $allmatches = get_object_vars(json_decode(file_get_contents("https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/382991?api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
+                foreach($allmatches as $matches){
+                    foreach($matches as $match){
+                        $match = get_object_vars($match);
+                        //if($match['queueType'] === "URF_5x5"){
+                            echo "<br /><br />" . $match['queueType'] . "<br />";
+                            echo "<br /><br />" . $match['queueType'] . "<br />";
+                            print_r(array_values($match));
+
+                        //}
+                    }
+                    print_r($matches . "<br />");
                 }
-                print_r($matches . "<br />");
-            }
-            /***
-             * 
-             * GET CHAMPION ID, NAME, SPELLS 
-             * 
-             */
-            $staticchampdata = get_object_vars(json_decode(file_get_contents("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
-            foreach($staticchampdata as $champdata){
-                if(is_object($champdata)){
-                    $champdata = get_object_vars($champdata);
-                    foreach($champdata as $champvalue){
-                        $champvalue = get_object_vars($champvalue);
-                        $champion[$champvalue['id']] = $champvalue;
-                        //var_dump($champvalue);
-                        foreach($champvalue as $championSpells){
-                            if(is_array($championSpells)){
-                            //print_r($championSpells[0]);
-                            //echo "<br /><br />";
-                            $championsSpells[$champvalue['id']][1]['name'] = get_object_vars($championSpells[0])['name'];
-                            $championsSpells[$champvalue['id']][2]['name'] = get_object_vars($championSpells[1])['name'];
-                            $championsSpells[$champvalue['id']][3]['name'] = get_object_vars($championSpells[2])['name'];
-                            $championsSpells[$champvalue['id']][4]['name'] = get_object_vars($championSpells[3])['name'];
+                /***
+                 * 
+                 * GET CHAMPION ID, NAME, SPELLS 
+                 * 
+                 */
+                $staticchampdata = get_object_vars(json_decode(file_get_contents("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
+                foreach($staticchampdata as $champdata){
+                    if(is_object($champdata)){
+                        $champdata = get_object_vars($champdata);
+                        foreach($champdata as $champvalue){
+                            $champvalue = get_object_vars($champvalue);
+                            $champion[$champvalue['id']] = $champvalue;
+                            //var_dump($champvalue);
+                            foreach($champvalue as $championSpells){
+                                if(is_array($championSpells)){
+                                //print_r($championSpells[0]);
+                                //echo "<br /><br />";
+                                $championsSpells[$champvalue['id']][1]['name'] = get_object_vars($championSpells[0])['name'];
+                                $championsSpells[$champvalue['id']][2]['name'] = get_object_vars($championSpells[1])['name'];
+                                $championsSpells[$champvalue['id']][3]['name'] = get_object_vars($championSpells[2])['name'];
+                                $championsSpells[$champvalue['id']][4]['name'] = get_object_vars($championSpells[3])['name'];
+                                }
                             }
                         }
                     }
                 }
-            }
-            /***
-             * 
-             * GET ALL ITEMS AND ITEM DATA
-             * 
-             */
-            $allitems = get_object_vars(json_decode(file_get_contents("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=image&api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
-            foreach($allitems as $itemdata){
-                //var_dump($itemdata);
-                if(is_object($itemdata)){
-                foreach($itemdata as $id => $item){
-                    if(is_numeric($id)){
-                    $item = get_object_vars($item);
-                    //var_dump($item);
-                    $items[$id] = $item;
+                /***
+                 * 
+                 * GET ALL ITEMS AND ITEM DATA
+                 * 
+                 */
+                $allitems = get_object_vars(json_decode(file_get_contents("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=image&api_key=79de72ae-b73d-4f43-ad31-4267915265ea")));
+                foreach($allitems as $itemdata){
+                    //var_dump($itemdata);
+                    if(is_object($itemdata)){
+                    foreach($itemdata as $id => $item){
+                        if(is_numeric($id)){
+                        $item = get_object_vars($item);
+                        //var_dump($item);
+                        $items[$id] = $item;
+                        }
                     }
-                }
+                    }
                 }
             }
          ?>
